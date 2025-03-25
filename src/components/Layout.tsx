5c403dd722b5c322a8aacde974ab49c28a1fc56d
PedrOliveiraM <pedropucmont@gmail.com>
2025-03-25 09:33:12 -0300

    feat: enhance layout component with padding and responsive child container

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,15 +1,14 @@
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
-import { AppSidebar } from "./sidebar/app-sidebar"
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "./sidebar/app-sidebar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
       <AppSidebar />
       <SidebarTrigger />
-      <main className="flex justify-center items-center min-h-screen">
-        {children}
+      <main className="flex justify-center items-center min-h-screen p-5">
+        <div className="w-full max-w-5xl overflow-auto">{children}</div>
       </main>
     </SidebarProvider>
-  )
+  );
 }
-
