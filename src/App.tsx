2c7ec341d70a37b872863893b8aea6864d245ed3
PedrOliveiraM <pedropucmont@gmail.com>
2025-03-24 22:50:35 -0300

    feat: add ESLint configuration and update App component layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
+import Layout from "./components/Layout"
 import { UserTable } from "./components/user-table"
 
 function App() {
   return (
-    <div className="container mx-auto py-10 bg-[var(--background)]">
-      <h1 className="text-2xl font-bold mb-5">Users</h1>
-      <div className="bg-gray-200">
+    <Layout>
       <UserTable/>
-      </div>
-    </div>
+    </Layout>
   )
 }
 
