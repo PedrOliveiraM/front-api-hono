a990d96468c4e389ba2080dfdc069e76986e61a8
PedrOliveiraM <pedropucmont@gmail.com>
2025-03-25 21:27:19 -0300

    feat: add DTOs for API response and user organization management

---
+++ src/@types/user-organization/user-organization-dto.ts
@@ -0,0 +1,10 @@
+export interface UserOrganizationDto {
+  userId: string
+  organizationId: string
+  userOrganizationId: string
+  isActive: boolean
+  userName: string
+  email: string
+  organizationName: string
+  createdAt: string
+}
---
+++ src/@types/user/user-dto.ts
@@ -0,0 +1,9 @@
+export type UserDto = { data: User[] }
+
+type User = {
+  id: string
+  name: string
+  email: string
+  createdAt: string
+  updatedAt: string
+}
