8b2939e7916972d113532f4bcbcbd3655ce82fff
PedrOliveiraM <pedropucmont@gmail.com>
2025-03-25 21:26:00 -0300

    feat: update user columns table to use UserOrganizationDto and enhance date formatting

--- src/components/user-columns-table.tsx
+++ src/components/user-columns-table.tsx
@@ -1,18 +1,19 @@
-import { Person } from "@/mock/makeData";
+import { UserOrganizationDto } from "@/@types/user-organization/user-organization-dto";
 import { ColumnDef } from "@tanstack/react-table";
+import { format } from "date-fns";
 import { Badge } from "./ui/badge";
 
-export const columns: ColumnDef<Person>[] = [
+export const columns: ColumnDef<UserOrganizationDto>[] = [
   {
     header: "Tabela de Usuários",
     footer: (props) => props.column.id,
     columns: [
       {
-        accessorKey: "name",
-        accessorFn: (row) => row.name,
+        accessorKey: "userName",
+        accessorFn: (row) => row.userName,
         header: () => "Nome Completo",
         cell: (info) => info.getValue(),
-        enableSorting: true, // Habilita ordenação
+        enableSorting: true,
         footer: (props) => props.column.id,
       },
       {
@@ -21,7 +22,7 @@ export const columns: ColumnDef<Person>[] = [
         footer: (props) => props.column.id,
       },
       {
-        accessorKey: "status",
+        accessorKey: "isActive",
         header: () => "Status",
         footer: (props) => props.column.id,
         cell: (info) => {
@@ -40,14 +41,18 @@ export const columns: ColumnDef<Person>[] = [
         }
       },
       {
-        accessorKey: "organization",
+        accessorKey: "organizationName",
         header: "Organização",
         footer: (props) => props.column.id,
       },
       {
-        accessorKey: "createAt",
+        accessorKey: "createdAt",
         header: "Data de Criação",
         footer: (props) => props.column.id,
+        cell: (info) => {
+          const date = new Date(info.getValue());
+          return format(date, "dd/MM/yyyy");
+        }
       },
     ],
   }
